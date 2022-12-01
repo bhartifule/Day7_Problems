a25c149b15a186c6c39b66f23425c1ede926d86e
Bharti <bharatifule10gmail.com>
2022-12-01 15:48:50 +0530

    Display current Board added

--- bridgelabz/Tic_Toc_Tie.java
+++ bridgelabz/Tic_Toc_Tie.java
@@ -3,26 +3,25 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class Tic_Toc_Tie {
-	public static void main(String[] args) {
+public static void main(String[] args) {
+	
+
         System.out.println("Welcome in tic tac toy ");
        createEmptyBoard();
        allowPlayer();
+       showBoard();
     }
     public static void createEmptyBoard(){
-        char[] sizeOfBoard = new char[10];
+
         System.out.println(" ____________");
         for (int i = 1; i <10; i++){
             System.out.print("|___");
             if (i % 3 == 0) {
                 System.out.println("|");
             }
-        
-            
-            
-            
-        }}
+        }
 
-    
+    }
     public static void allowPlayer(){
         System.out.println("Enter your choice if wont 'X' then press 1 & if you wont 'O' then press 2");
         Scanner sc = new Scanner(System.in);
@@ -35,4 +34,15 @@ public class Tic_Toc_Tie {
             System.out.println("Computer => X");
         }
 
-    }}
+    }
+    public static void showBoard(){
+        char[] arr = {48,49,50,51,52,53,54,55,56,57};
+        System.out.println(" ____________");
+        for (int i = 1; i <=9; i++){
+            System.out.print("|_"+arr[i]+"_");
+            if (i % 3 == 0) {
+                System.out.println("|");
+            }
+        }
+    }
+    }
