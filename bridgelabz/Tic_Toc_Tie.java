c9860acfae08af28d374a302b99c2b06ad6ac045
Bharti <bharatifule10gmail.com>
2022-12-01 15:38:46 +0530

    EmptyBoard added

---
+++ bridgelabz/Tic_Toc_Tie.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+public class Tic_Toc_Tie {
+	public static void main(String[] args) {
+        System.out.println("Welcome in tic tac toy ");
+       createEmptyBoard();
+    }
+    public static void createEmptyBoard(){
+        char[] sizeOfBoard = new char[10];
+        System.out.println(" ____________");
+        for (int i = 1; i <10; i++){
+            System.out.print("|___");
+            if (i % 3 == 0) {
+                System.out.println("|");
+            }
+
+        }
+       
+    }
+}
